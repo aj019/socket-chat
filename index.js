4aa3c4307573e5d5490efd0cb57c57bf7102de0b
Anuj Gupta <junagupta19@gmail.com>
2018-07-14 11:35:59 +0530

    Created a server and connected clients using socket

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
   app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
   });
 
+
+  io.on('connection', function(socket){
+    console.log('a user connected');
+  });
+  
   http.listen(3333, function(){
     console.log('listening on *:3333');
   });
